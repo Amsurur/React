3c46e2726dd042d0abd691c36be84d1ca1827f1e
Muhammadsurur Abdulloev <abdulloevmuhammadsurur@gmail.com>
2025-07-17 15:40:30 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Api } from "./config/api";
 import { useEffect, useState } from "react";
 import TodoTable from "./components/TodoTable";
 import AddModal from "./components/AddModal";
+import useDarkSide from "./config/useDarkMode";
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [theme, toggleTheme] = useDarkSide();
+
   const [openModal, setOpenModal] = useState(false);
   async function GetData() {
     try {
@@ -30,8 +33,13 @@ const App = () => {
     GetData();
   }, []);
   return (
-    <div>
-      <button onClick={() => setOpenModal(true)}>Add New Todo</button>
+    <div className="">
+      <button
+        onClick={toggleTheme}
+        className="px-4 py-2 rounded bg-gray-800 text-white dark:bg-yellow-400 dark:text-black"
+      >
+        Toggle Theme
+      </button>
       {openModal && <AddModal AddData={AddData} />}
       <TodoTable data={data} />
     </div>
